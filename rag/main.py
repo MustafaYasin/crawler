d24e8de8f58900a41da4341a01596e13a7768499
yasin-cai <mustafa.yasin@convalid-ai.de>
2025-03-20 15:17:53 +0100

    output file in markdown

--- rag/main.py
+++ rag/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from rag!")
+from docling.document_converter import DocumentConverter
 
+source = "/Users/mustafayasin/Desktop/2411.14974v2.pdf"
+converter = DocumentConverter()
+print("I was here")
+result = converter.convert(source)
 
-if __name__ == "__main__":
-    main()
+# Save to a file instead of printing to terminal
+output_path = "/Users/mustafayasin/Desktop/converted_document.md"
+with open(output_path, "w", encoding="utf-8") as file:
+    file.write(result.document.export_to_markdown())
+
+print(f"Markdown saved to {output_path}")
